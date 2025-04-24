ac3cd07629bbe8e7a3cf1bcc43a698d2bf6fd297
Orçun Aşık <asikorcun@gmail.com>
2025-04-24 19:26:06 +0300

    Created entities for Category,MenuItem,User

---
+++ Core/CafeAPI.Domain/Entities/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace CafeAPI.Domain.Entities;
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<MenuItem> MenuItems { get; set; }
+}
---
+++ Core/CafeAPI.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CafeAPI.Domain.Entities;
+public class User
+{
+    public int Id { get; set; }
+    public string AppUserId { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Role { get; set; }
+}
