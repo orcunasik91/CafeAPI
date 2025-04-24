30afb3e5b54fff3ea1c36f5eca18ee0542d6c50e
Orçun Aşık <asikorcun@gmail.com>
2025-04-24 19:26:44 +0300

    Created MenuItemDtos

---
+++ Core/CafeAPI.Application/Dtos/MenuItemDtos/DetailMenuItemDto.cs
@@ -0,0 +1,14 @@
+﻿using CafeAPI.Application.Dtos.CategoryDtos;
+
+namespace CafeAPI.Application.Dtos.MenuItemDtos;
+public class DetailMenuItemDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+    public bool IsAvailable { get; set; }
+    public int CategoryId { get; set; }
+    public ResultCategoryDto Category { get; set; }
+}
