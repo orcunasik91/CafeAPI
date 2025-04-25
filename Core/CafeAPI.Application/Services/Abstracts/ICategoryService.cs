955ddc20f16887f31d02fc565b7a283cf8ed2f03
Orçun Aşık <asikorcun@gmail.com>
2025-04-25 21:29:42 +0300

    Created concracts for Category and MenuItem

---
+++ Core/CafeAPI.Application/Services/Abstracts/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using CafeAPI.Application.Dtos.CategoryDtos;
+
+namespace CafeAPI.Application.Services.Abstracts;
+public interface ICategoryService
+{
+    Task<List<ResultCategoryDto>> GetAllCategoriesAsync();
+    Task<DetailCategoryDto> GetByIdCategoryAsync(int id);
+    Task AddCategoryAsync(CreateCategoryDto categoryDto);
+    Task UpdateCategoryAsync(UpdateCategoryDto categoryDto);
+    Task RemoveCategoryAsync(int id);
+}
---
+++ Core/CafeAPI.Application/Services/Abstracts/IMenuItemService.cs
@@ -0,0 +1,12 @@
+﻿using CafeAPI.Application.Dtos.CategoryDtos;
+using CafeAPI.Application.Dtos.MenuItemDtos;
+
+namespace CafeAPI.Application.Services.Abstracts;
+public interface IMenuItemService
+{
+    Task<List<ResultMenuItemDto>> GetAllMenuItemsAsync();
+    Task<DetailMenuItemDto> GetByIdMenuItemAsync(int id);
+    Task AddMenuItemAsync(CreateMenuItemDto menuItemDto);
+    Task UpdateMenuItemAsync(UpdateMenuItemDto menuItemDto);
+    Task RemoveMenuItemAsync(int id);
+}
