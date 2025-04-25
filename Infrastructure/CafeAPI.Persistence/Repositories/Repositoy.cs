61fea18482b292ad2c93d8b50c694378bf6b82c7
Orçun Aşık <asikorcun@gmail.com>
2025-04-25 21:28:47 +0300

    Created Repository class and implemented IRepositoy

---
+++ Infrastructure/CafeAPI.Persistence/Repositories/Repositoy.cs
@@ -0,0 +1,37 @@
+﻿using CafeAPI.Application.Interfaces;
+using CafeAPI.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CafeAPI.Persistence.Repositories;
+public class Repositoy<TEntity>(AppDbContext _context) : IRepository<TEntity> where TEntity : class, new()
+{
+    public async Task AddAsync(TEntity entity)
+    {
+        await _context.Set<TEntity>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<TEntity>> GetAllAsync()
+    {
+        return await _context.Set<TEntity>().ToListAsync();
+    }
+
+    public async Task<TEntity> GetByIdAsync(int id)
+    {
+        return await _context.Set<TEntity>().FindAsync(id);
+
+    }
+
+    public async Task RemoveAsync(TEntity entity)
+    {
+        _context.Set<TEntity>().Remove(entity);
+        await _context.SaveChangesAsync();
+
+    }
+
+    public async Task UpdateAsync(TEntity entity)
+    {
+        _context.Set<TEntity>().Update(entity);
+        await _context.SaveChangesAsync();
+    }
+}
