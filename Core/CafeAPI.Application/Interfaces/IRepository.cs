fd00825ced9b73f14965aa2557b73a3966043e9a
Orçun Aşık <asikorcun@gmail.com>
2025-04-25 21:27:51 +0300

    Created IRepository interface

---
+++ Core/CafeAPI.Application/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace CafeAPI.Application.Interfaces;
+public interface IRepository<TEntity> where TEntity : class, new()
+{
+    Task<TEntity> GetByIdAsync(int id);
+    Task<List<TEntity>> GetAllAsync();
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task RemoveAsync(TEntity entity);
+}
