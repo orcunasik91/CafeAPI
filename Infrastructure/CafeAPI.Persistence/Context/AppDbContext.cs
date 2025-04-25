3520f4763ab4c124fbe9951089376ec52749522f
Orçun Aşık <asikorcun@gmail.com>
2025-04-25 21:27:17 +0300

    Created AppDbContext

---
+++ Infrastructure/CafeAPI.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CafeAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CafeAPI.Persistence.Context;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<MenuItem> MenuItems { get; set; }
+}
