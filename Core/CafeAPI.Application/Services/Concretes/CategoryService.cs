50dbc2b0da9713edffccdb091685e9bd23e19e4a
Orçun Aşık <asikorcun@gmail.com>
2025-04-25 21:33:27 +0300

    Created service classes to Category and MenuItem

---
+++ Core/CafeAPI.Application/Services/Concretes/CategoryService.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using CafeAPI.Application.Dtos.CategoryDtos;
+using CafeAPI.Application.Interfaces;
+using CafeAPI.Application.Services.Abstracts;
+using CafeAPI.Domain.Entities;
+
+namespace CafeAPI.Application.Services.Concretes;
+public class CategoryService : ICategoryService
+{
+    private readonly IRepository<Category> _categoryRepository;
+    private readonly IMapper _mapper;
+    public CategoryService(IRepository<Category> categoryRepository, IMapper mapper)
+    {
+        _categoryRepository = categoryRepository;
+        _mapper = mapper;
+    }
+    public async Task AddCategoryAsync(CreateCategoryDto categoryDto)
+    {
+        var category = _mapper.Map<Category>(categoryDto);
+        await _categoryRepository.AddAsync(category);
+    }
+    public async Task<List<ResultCategoryDto>> GetAllCategoriesAsync()
+    {
+        var categories = await _categoryRepository.GetAllAsync();
+        var result = _mapper.Map<List<ResultCategoryDto>>(categories);
+        return result;
+    }
+    public async Task<DetailCategoryDto> GetByIdCategoryAsync(int id)
+    {
+        var category = await _categoryRepository.GetByIdAsync(id);
+        var result = _mapper.Map<DetailCategoryDto>(category);
+        return result;
+    }
+    public async Task RemoveCategoryAsync(int id)
+    {
+        var category = await _categoryRepository.GetByIdAsync(id);
+        await _categoryRepository.RemoveAsync(category);
+    }
+    public async Task UpdateCategoryAsync(UpdateCategoryDto categoryDto)
+    {
+        var category = _mapper.Map<Category>(categoryDto);
+        await _categoryRepository.UpdateAsync(category);
+    }
+}
---
+++ Core/CafeAPI.Application/Services/Concretes/MenuItemService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using CafeAPI.Application.Dtos.CategoryDtos;
+using CafeAPI.Application.Dtos.MenuItemDtos;
+using CafeAPI.Application.Interfaces;
+using CafeAPI.Application.Services.Abstracts;
+using CafeAPI.Domain.Entities;
+
+namespace CafeAPI.Application.Services.Concretes;
+public class MenuItemService : IMenuItemService
+{
+    private readonly IRepository<MenuItem> _menuItemRepository;
+    private readonly IMapper _mapper;
+    public MenuItemService(IRepository<MenuItem> menuItemRepository, IMapper mapper)
+    {
+        _menuItemRepository = menuItemRepository;
+        _mapper = mapper;
+    }
+    public async Task AddMenuItemAsync(CreateMenuItemDto menuItemDto)
+    {
+        var menuItem = _mapper.Map<MenuItem>(menuItemDto);
+        await _menuItemRepository.AddAsync(menuItem);
+    }
+
+    public async Task<List<ResultMenuItemDto>> GetAllMenuItemsAsync()
+    {
+        var menuItems = await _menuItemRepository.GetAllAsync();
+        var result = _mapper.Map<List<ResultMenuItemDto>>(menuItems);
+        return result;
+    }
+
+    public async Task<DetailMenuItemDto> GetByIdMenuItemAsync(int id)
+    {
+        var menuItem = await _menuItemRepository.GetByIdAsync(id);
+        var result = _mapper.Map<DetailMenuItemDto>(menuItem);
+        return result;
+    }
+
+    public async Task RemoveMenuItemAsync(int id)
+    {
+        var menuItem = await _menuItemRepository.GetByIdAsync(id);
+        await _menuItemRepository.RemoveAsync(menuItem);
+    }
+
+    public async Task UpdateMenuItemAsync(UpdateMenuItemDto menuItemDto)
+    {
+        var menuItem = _mapper.Map<MenuItem>(menuItemDto);
+        await _menuItemRepository.UpdateAsync(menuItem);
+    }
+}
