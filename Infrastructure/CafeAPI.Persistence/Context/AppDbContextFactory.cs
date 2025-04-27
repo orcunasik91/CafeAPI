c9ab39249fe3f97da8eccc9afb7c8c0d2465c394
Orçun Aşık <asikorcun@gmail.com>
2025-04-27 21:51:29 +0300

    Created AppDbContextFactory class to avoid design errors during the migration process

---
+++ Infrastructure/CafeAPI.Persistence/Context/AppDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace CafeAPI.Persistence.Context;
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(),"../../Presentation/CafeAPI.WebApi"))
+            .AddJsonFile("appsettings.json")
+            .Build();
+        var connectionString = configuration.GetConnectionString("SqlConnection");
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
