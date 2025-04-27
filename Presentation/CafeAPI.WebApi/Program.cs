d662603cf88369a573b97c41559188fb8c00eb59
Orçun Aşık <asikorcun@gmail.com>
2025-04-27 16:38:48 +0300

    services registered to IoCAutoMapper, Repository, CategoryService services and Scalar as middleware registered have been to IoC

--- Presentation/CafeAPI.WebApi/Program.cs
+++ Presentation/CafeAPI.WebApi/Program.cs
@@ -1,21 +1,38 @@
+using CafeAPI.Application.Interfaces;
+using CafeAPI.Application.Mappings;
+using CafeAPI.Application.Services.Abstracts;
+using CafeAPI.Application.Services.Concretes;
 using CafeAPI.Persistence.Context;
+using CafeAPI.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
+    options.UseSqlServer
+    (builder.Configuration.GetConnectionString("SqlConnection"),
+    sqlOptions => sqlOptions.EnableRetryOnFailure()));
+
+builder.Services.AddAutoMapper(typeof(GeneralMapping));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repositoy<>));
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(opt =>
+    {
+        opt.Title = "Cafe API v1";
+        opt.Theme = ScalarTheme.Purple;
+        opt.DefaultHttpClient = new(ScalarTarget.Http, ScalarClient.Http1);
+    });
 }
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
