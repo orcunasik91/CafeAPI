71da3dc01beabcb1938f4b71610275bf49cb7a70
Orçun Aşık <asikorcun@gmail.com>
2025-04-29 21:04:27 +0300

    Created error codes for errormessage in responsedto

---
+++ Core/CafeAPI.Application/Dtos/ResponseDtos/ErrorCodes.cs
@@ -0,0 +1,8 @@
+﻿namespace CafeAPI.Application.Dtos.ResponseDtos;
+public static class ErrorCodes
+{
+    public const string NotFound = "NOT_FOUND";
+    public const string Unauthorized = "UNAUTHHORIZED";
+    public const string Exception = "EXCEPTION";
+    public const string ValidationError = "VALIDATION_ERROR";
+}
