d90a4969e6112cd71b5eeb97cbb46d111b444ff3
Orçun Aşık <asikorcun@gmail.com>
2025-05-05 20:33:37 +0300

    Created category validators for add and update operations

---
+++ Core/CafeAPI.Application/Validators/Category/AddCategoryValidator.cs
@@ -0,0 +1,13 @@
+﻿using CafeAPI.Application.Dtos.CategoryDtos;
+using FluentValidation;
+
+namespace CafeAPI.Application.Validators.Category;
+public class AddCategoryValidator : AbstractValidator<CreateCategoryDto>
+{
+    public AddCategoryValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty().WithMessage("Kategori Adı Boş Geçilmemelidir!")
+            .Length(3, 30).WithMessage("Kategori Adı Uzunluğu 3 ile 30 Karakter Arasında Olmalıdır!");
+    }
+}
---
+++ Core/CafeAPI.Application/Validators/Category/UpdateCategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using CafeAPI.Application.Dtos.CategoryDtos;
+using FluentValidation;
+
+namespace CafeAPI.Application.Validators.Category;
+public class UpdateCategoryValidator : AbstractValidator<UpdateCategoryDto>
+{
+    public UpdateCategoryValidator()
+    {
+        RuleFor(c => c.Id)
+            .NotEmpty().WithMessage("Kategori Id'si '0' Olmamalıdır!");
+        RuleFor(c => c.Name)
+            .NotEmpty().WithMessage("Kategori Adı Boş Geçilmemelidir!")
+            .Length(3, 30).WithMessage("Kategori Adı Uzunluğu 3 ile 30 Karakter Arasında Olmalıdır!");
+    }
+}
