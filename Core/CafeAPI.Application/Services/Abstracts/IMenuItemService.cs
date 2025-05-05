1a483e6ff4948126549708a04346acde135cdde9
Orçun Aşık <asikorcun@gmail.com>
2025-05-05 20:35:57 +0300

    Methods' types of return changed

--- Core/CafeAPI.Application/Services/Abstracts/IMenuItemService.cs
+++ Core/CafeAPI.Application/Services/Abstracts/IMenuItemService.cs
@@ -1,12 +1,13 @@
 ﻿using CafeAPI.Application.Dtos.CategoryDtos;
 using CafeAPI.Application.Dtos.MenuItemDtos;
+using CafeAPI.Application.Dtos.ResponseDtos;
 
 namespace CafeAPI.Application.Services.Abstracts;
 public interface IMenuItemService
 {
-    Task<List<ResultMenuItemDto>> GetAllMenuItemsAsync();
-    Task<DetailMenuItemDto> GetByIdMenuItemAsync(int id);
-    Task AddMenuItemAsync(CreateMenuItemDto menuItemDto);
-    Task UpdateMenuItemAsync(UpdateMenuItemDto menuItemDto);
-    Task RemoveMenuItemAsync(int id);
+    Task<ResponseDto<List<ResultMenuItemDto>>> GetAllMenuItemsAsync();
+    Task<ResponseDto<DetailMenuItemDto>> GetByIdMenuItemAsync(int id);
+    Task<ResponseDto<MenuItemResponseDto>> AddMenuItemAsync(CreateMenuItemDto menuItemDto);
+    Task<ResponseDto<object>> UpdateMenuItemAsync(UpdateMenuItemDto menuItemDto);
+    Task<ResponseDto<object>> RemoveMenuItemAsync(int id);
 }
