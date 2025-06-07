81bae1ae598ce79af4ad446db4889f176df6b120
Orçun Aşık <asikorcun@gmail.com>
2025-06-08 00:44:55 +0300

    Table model created

---
+++ Core/CafeAPI.Domain/Entities/Table.cs
@@ -0,0 +1,8 @@
+﻿namespace CafeAPI.Domain.Entities;
+public class Table
+{
+    public int Id { get; set; }
+    public int TableNumber { get; set; }
+    public int Capacity { get; set; }
+    public bool IsActive { get; set; }
+}
