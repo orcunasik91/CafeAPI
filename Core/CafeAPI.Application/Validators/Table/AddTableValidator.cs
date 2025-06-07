23e7d5c5981b437e8941f444b2989a95dc2792db
Orçun Aşık <asikorcun@gmail.com>
2025-06-08 00:47:44 +0300

    Validators created for Table model

---
+++ Core/CafeAPI.Application/Validators/Table/AddTableValidator.cs
@@ -0,0 +1,16 @@
+﻿using CafeAPI.Application.Dtos.TableDtos;
+using FluentValidation;
+
+namespace CafeAPI.Application.Validators.Table;
+public class AddTableValidator : AbstractValidator<CreateTableDto>
+{
+    public AddTableValidator()
+    {
+        RuleFor(t => t.TableNumber)
+            .NotEmpty().WithMessage("Masa Numarası Boş Olmamalıdır")
+            .GreaterThan(0).WithMessage("Masa Numarası 0'dan Büyük Olmalıdır");
+        RuleFor(t => t.Capacity)
+            .NotEmpty().WithMessage("Masa Kapasitesi Boş Olmamalıdır")
+            .GreaterThan(0).WithMessage("Masa Kapasitesi 0'dan Büyük Olmalıdır");
+    }
+}
---
+++ Core/CafeAPI.Application/Validators/Table/UpdateTableValidator.cs
@@ -0,0 +1,18 @@
+﻿using CafeAPI.Application.Dtos.TableDtos;
+using FluentValidation;
+
+namespace CafeAPI.Application.Validators.Table;
+public class UpdateTableValidator : AbstractValidator<UpdateTableDto>
+{
+    public UpdateTableValidator()
+    {
+        RuleFor(t => t.Id)
+            .NotEmpty().WithMessage("Masa ID'si Boş Geçilmemelidir");
+        RuleFor(t => t.TableNumber)
+            .NotEmpty().WithMessage("Masa Numarası Boş Olmamalıdır")
+            .GreaterThan(0).WithMessage("Masa Numarası 0'dan Büyük Olmalıdır");
+        RuleFor(t => t.Capacity)
+            .NotEmpty().WithMessage("Masa Kapasitesi Boş Olmamalıdır")
+            .GreaterThan(0).WithMessage("Masa Kapasitesi 0'dan Büyük Olmalıdır");
+    }
+}
