4fe67849f68f5cafff3f5a839b3ba78e00c25516
Orçun Aşık <asikorcun@gmail.com>
2025-06-08 00:44:35 +0300

    TableDtos created

---
+++ Core/CafeAPI.Application/Dtos/TableDtos/CreateTableDto.cs
@@ -0,0 +1,7 @@
+﻿namespace CafeAPI.Application.Dtos.TableDtos;
+public class CreateTableDto
+{
+    public int TableNumber { get; set; }
+    public int Capacity { get; set; }
+    public bool IsActive { get; set; }
+}
---
+++ Core/CafeAPI.Application/Dtos/TableDtos/UpdateTableDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CafeAPI.Application.Dtos.TableDtos;
+public class UpdateTableDto
+{
+    public int Id { get; set; }
+    public int TableNumber { get; set; }
+    public int Capacity { get; set; }
+    public bool IsActive { get; set; }
+}
