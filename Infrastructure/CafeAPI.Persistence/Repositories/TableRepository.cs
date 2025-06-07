3ebc9e8d64819a501b496b4b244a4744ed79dc5a
Orçun Aşık <asikorcun@gmail.com>
2025-06-08 00:48:03 +0300

    TableRepository added

---
+++ Infrastructure/CafeAPI.Persistence/Repositories/TableRepository.cs
@@ -0,0 +1,20 @@
+﻿using CafeAPI.Application.Interfaces;
+using CafeAPI.Domain.Entities;
+using CafeAPI.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CafeAPI.Persistence.Repositories;
+public class TableRepository(AppDbContext _context) : ITableRepository
+{
+    public async Task<List<Table>> GetActiveTablesAsync()
+    {
+        var result = await _context.Tables.Where(t => t.IsActive == true).ToListAsync();
+        return result;
+    }
+
+    public async Task<Table> GetByTableNumberAsync(int tableNumber)
+    {
+        var result = await _context.Tables.FirstOrDefaultAsync(t => t.TableNumber == tableNumber);
+        return result;
+    }
+}
