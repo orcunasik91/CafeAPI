d613680a47139d5f0fed429d0ed402b9047321f0
Orçun Aşık <asikorcun@gmail.com>
2025-06-08 00:42:42 +0300

    response dto and errorcodes edited

--- Core/CafeAPI.Application/Dtos/ResponseDtos/ErrorCodes.cs
+++ Core/CafeAPI.Application/Dtos/ResponseDtos/ErrorCodes.cs
@@ -5,4 +5,7 @@ public static class ErrorCodes
     public const string Unauthorized = "UNAUTHHORIZED";
     public const string Exception = "EXCEPTION";
     public const string ValidationError = "VALIDATION_ERROR";
+    public const string DuplicateError = "DUPLICATE_ERROR";
+    public const string Forbidden = "FORBIDDEN";
+    public const string BadRequest = "BAD_REQUEST";
 }
--- Core/CafeAPI.Application/Dtos/ResponseDtos/ResponseDto.cs
+++ Core/CafeAPI.Application/Dtos/ResponseDtos/ResponseDto.cs
@@ -4,5 +4,5 @@ public class ResponseDto<T> where T : class
     public bool Success { get; set; }
     public string? Message { get; set; }
     public T Data { get; set; }
-    public string? ErrorCodes { get; set; }
+    public string? ErrorCode { get; set; }
 }
